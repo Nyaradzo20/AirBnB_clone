59fcfc714dc1a58aa2ef4a70b1094b426807d139
Nyaradzo20 <nchinyati20@gmai.com>
2022-12-17 20:27:27 +0000

    add files

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Module for FileStorage autoinit."""
+"""This module instantiates an object of class FileStorage"""
 from models.engine.file_storage import FileStorage
+
+
 storage = FileStorage()
 storage.reload()
